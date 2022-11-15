1f05b54d2e68996d2d42d3547156c2669ca4c1e5
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-11-15 08:40:15 -0800

    nesting navigators

--- App.js
+++ App.js
@@ -2,12 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#cccccc'},
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25'},
+      }}
+    >
+      <Drawer.Screen 
+        name='categories'
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories'
+        }}
+      />
+      <Drawer.Screen name='Favorites' component={FavoritesScreen} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -22,10 +47,10 @@ export default function App() {
           }}
         >
           <Stack.Screen 
-            name='MealsCategories' 
-            component={CategoriesScreen} 
+            name='Drawer' 
+            component={DrawerNavigator} 
             options={{
-              title: 'All Categories',
+              headerShown: false,
             }} 
           />
           <Stack.Screen 
@@ -36,6 +61,9 @@ export default function App() {
           <Stack.Screen 
             name='MealDetail'
             component={MealDetailScreen}
+            options={{
+              title: 'About the Meal'
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
