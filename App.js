b9965b89886e9f4c686dee0ed863d989f2bbc1cf
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-11-15 08:56:44 -0800

    bottom tabs

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 
 import CategoriesScreen from './screens/CategoriesScreen';
@@ -20,16 +21,32 @@ function DrawerNavigator() {
         headerStyle: { backgroundColor: '#cccccc'},
         headerTintColor: 'white',
         sceneContainerStyle: { backgroundColor: '#3f2f25'},
+        drawerContentStyle: { backgroundColor: '#351401' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1'
       }}
     >
       <Drawer.Screen 
         name='categories'
         component={CategoriesScreen}
         options={{
-          title: 'All Categories'
+          title: 'All Categories',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name='list' color={color} size={size} />
+          ),
         }}
       />
-      <Drawer.Screen name='Favorites' component={FavoritesScreen} />
+      <Drawer.Screen 
+        name='Favorites' 
+        component={FavoritesScreen} 
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({color, size}) => (
+            <Ionicons name='star' color={color} size={size} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   )
 }
@@ -43,7 +60,7 @@ export default function App() {
           screenOptions={{
             headerStyle: { backgroundColor: '#cccccc'},
             headerTintColor: 'white',
-            contentStyle: { backgroundColor: '#3f2f25'}
+            contentStyle: { backgroundColor: '#3f2f25'},
           }}
         >
           <Stack.Screen 
